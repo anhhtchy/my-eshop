b9ebf263d8a8d2ac85887e2f102d63b0e83c0eea
anhhtchy <anhhtchy@gmail.com>
2022-04-06 22:27:17 +0700

    refactor project folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ItemDetailComponent } from './components/item-detail/item-detail.component';
-import { ItemsComponent } from './components/items/items.component';
+import { ItemDetailComponent } from './components/public/item-detail/item-detail.component';
+import { ItemsComponent } from './components/public/items/items.component';
 
 const routes: Routes = [
   {path: '', component: ItemsComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ItemDetailComponent } from './components/item-detail/item-detail.component';
-import { ItemsComponent } from './components/items/items.component';
+import { ItemDetailComponent } from './components/public/item-detail/item-detail.component';
+import { ItemsComponent } from './components/public/items/items.component';
 
 @NgModule({
   declarations: [
